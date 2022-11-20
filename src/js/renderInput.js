1862b0bb1e7783ed25501b9b19a59451fb045aab
vsevolodjarkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-11-20 02:27:14 +0200

    save

--- src/js/renderInput.js
+++ src/js/renderInput.js
@@ -1,23 +1,24 @@
 const formSelected = (name) => {
-	console.log(name === 'Type of Inquiry*');
+	const res = name === 'Type of Inquiry*';
+	console.log(res);
 	return `
-	<div class=${(name === 'Type of Inquiry*') ? 'form-group' : 'form-group' + ' ' + 'form-group__last-drid'}>
-	<div class="drop-down">
-	<button class="drop-down__button">
-		${name}
-		<svg class="drop-down__svg">
-			<use href="./svg/symbol-defs.svg#icon-Shape"></use>
-		</svg>
-	</button>
-		<ul class="drop-down__list">
-				<li class="drop-down__list-item" data-value="Option 1">Option 1</li>
-				<li class="drop-down__list-item" data-value="Option 2">Option 2</li>
-				<li class="drop-down__list-item" data-value="Option 3">Option 3</li>
-				<li class="drop-down__list-item" data-value="Option 4">Option 4</li>
-				<li class="drop-down__list-item" data-value="Option 5">Option 5</li>
-		</ul>
-		<input class="drop-down__input-hidden" type="text" name="options" value="">
-	</div>
+	<div class=${res ? 'form-group__last-grid' : 'form-group' }>
+		<div class="drop-down">
+			<button class="drop-down__button">
+				${name}
+				<svg class=${ res ? 'drop-down__svg-last' : 'drop-down__svg' }>
+					<use href="./svg/symbol-defs.svg#icon-Shape"></use>
+				</svg>
+			</button>
+			<ul class="drop-down__list">
+					<li class="drop-down__list-item" data-value="Option 1">Option 1</li>
+					<li class="drop-down__list-item" data-value="Option 2">Option 2</li>
+					<li class="drop-down__list-item" data-value="Option 3">Option 3</li>
+					<li class="drop-down__list-item" data-value="Option 4">Option 4</li>
+					<li class="drop-down__list-item" data-value="Option 5">Option 5</li>
+			</ul>
+			<input class="drop-down__input-hidden" type="text" name="options" value="">
+		</div>
 	</div>
 	`;
 };
